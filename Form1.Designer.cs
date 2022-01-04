95df40c7736647fb3ba8ebad84b77ffe2ae5922e
Sama Hussein <hussein.sama104@outlook.com>
2022-01-05 00:22:11 +0200

    final touches.

---
+++ Form1.Designer.cs
@@ -0,0 +1,233 @@
+﻿
+namespace FCFS_SJF
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.label7 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(88, 365);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(20, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "P1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(88, 425);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(20, 15);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "P2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(397, 364);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(20, 15);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "P3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(397, 435);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(20, 15);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "P4";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.label5.Location = new System.Drawing.Point(155, 34);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(33, 15);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "RAM";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(125, 361);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 23);
+            this.textBox1.TabIndex = 6;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(125, 421);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 23);
+            this.textBox2.TabIndex = 7;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(435, 361);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 23);
+            this.textBox3.TabIndex = 8;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(435, 430);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 23);
+            this.textBox4.TabIndex = 9;
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.ForeColor = System.Drawing.Color.SaddleBrown;
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Location = new System.Drawing.Point(69, 68);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(219, 166);
+            this.checkedListBox1.TabIndex = 10;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(329, 68);
+            this.textBox5.Multiline = true;
+            this.textBox5.Name = "textBox5";
+            this.textBox5.ReadOnly = true;
+            this.textBox5.Size = new System.Drawing.Size(229, 166);
+            this.textBox5.TabIndex = 11;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.label6.Location = new System.Drawing.Point(425, 34);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(30, 15);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "CPU";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(674, 378);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(114, 39);
+            this.button1.TabIndex = 13;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(591, 68);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(433, 166);
+            this.listView1.TabIndex = 14;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(792, 34);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(13, 15);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "T";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1055, 515);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.checkedListBox1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FCFS_SJF
+{
+    public partial class Form1 : Form
+    {
+        // WITH SAME ARRIVAL TIME
+        static int n = 4;
+        static float avgTAT, avgWT;
+        int []Processes = {1,2,3,4};
+        int []BurstTime = new int[n];
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            listView1.View = View.Details;
+            listView1.GridLines = true;
+            listView1.Columns.Add("Processes", 70);
+            listView1.Columns.Add("Burst Time", 70);    // time required by process for execution
+            listView1.Columns.Add("Waiting Time", 90);
+            listView1.Columns.Add("TAT", 70);            // time spent waiting and execution
+
+        }
+
+        void SJF() {
+            //Shortest Job First Scheduling: less burst time --> executed first
+
+            List<int> list = new List<int>();
+            list.Add(int.Parse(textBox1.Text));
+            list.Add(int.Parse(textBox2.Text));
+            list.Add(int.Parse(textBox3.Text));
+            list.Add(int.Parse(textBox4.Text));
+
+            list.Sort();
+
+            checkedListBox1.Items.Add(list[0]);
+            checkedListBox1.Items.Add(list[1]);
+            checkedListBox1.Items.Add(list[2]);
+            checkedListBox1.Items.Add(list[3]);
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           /* //Processes added to the queue
+            checkedListBox1.Items.Add(textBox1.Text);
+            checkedListBox1.Items.Add(textBox2.Text);
+            checkedListBox1.Items.Add(textBox3.Text);
+            checkedListBox1.Items.Add(textBox4.Text); 
+           */
+            //Burst time of each process
+            BurstTime[0] = Convert.ToInt32(textBox1.Text);
+            BurstTime[1] = Convert.ToInt32(textBox2.Text);
+            BurstTime[2] = Convert.ToInt32(textBox3.Text);
+            BurstTime[3] = Convert.ToInt32(textBox4.Text);
+            SJF();
+
+            timer1.Start();
+            findsAvgTime(Processes,n,BurstTime);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+           // textBox5 ---> CPU
+            timer1.Interval = Convert.ToInt32(checkedListBox1.Items[0]) * 1000;
+
+            if (checkedListBox1.Items[0].ToString() == textBox1.Text) {
+                textBox5.Text = textBox1.Text;
+            }
+            else if (checkedListBox1.Items[0].ToString() == textBox2.Text)
+            {
+                timer1.Interval = Convert.ToInt32(textBox2.Text) * 1000;
+                textBox5.Text = textBox2.Text;  //CPU is allocated to this process 
+            }
+            else if (checkedListBox1.Items[0].ToString() == textBox3.Text)
+            {
+                timer1.Interval = Convert.ToInt32(textBox3.Text) * 1000;
+                textBox5.Text = textBox3.Text;
+            }
+            else if (checkedListBox1.Items[0].ToString() == textBox4.Text)
+            {
+                timer1.Interval = Convert.ToInt32(textBox4.Text) * 1000;
+                textBox5.Text = textBox4.Text;
+            }
+
+            checkedListBox1.Items.RemoveAt(0);
+            if (checkedListBox1.Items.Count <= 0) {
+                MessageBox.Show("Average Waiting Time: " + avgWT + "\n" + "Average TAT:" + avgTAT);
+                timer1.Stop();               
+            }
+        }
+
+        void findsAvgTime(int[] processes, int n, int[] BurstTime) {
+            int[] TAT = new int[n];
+            int[] Waiting = new int[n];
+            int totalWaiting = 0;
+            int totalTAT = 0;
+
+            findWaitingTime(processes, n, Waiting, BurstTime);
+            findTAT(processes, n, Waiting, BurstTime, TAT);
+
+            for (int i = 1; i < n; i++)
+            {
+                totalWaiting += Waiting[i];
+                totalTAT += TAT[i];
+                string[] Row = { (i + 1).ToString(), BurstTime[i].ToString(), Waiting[i].ToString(), TAT[i].ToString() };
+                var Item = new ListViewItem(Row);
+                listView1.Items.Add(Item); 
+            }
+            avgWT = totalWaiting / n;   //average waiting time taken by each process
+            avgTAT = totalTAT / n;      //average turn around time taken by each process
+        }
+
+        void findWaitingTime(int[]processes, int n, int []Waiting, int[]BurstTime) {
+            Waiting[0] = 0;          //first process will execute immediately
+            for (int i = 1; i < n; i++) {
+                Waiting[i] = BurstTime[i - 1] + Waiting[i - 1]; //calculating the waiting time of each process = BT+WT of the previous process
+            }
+        }
+
+        void findTAT(int[] processes, int n, int[] Waiting, int[] BurstTime, int[]TAT) {
+            TAT[0] = BurstTime[0];
+            for (int i = 1; i < n; i++) {
+                TAT[i] = BurstTime[i] + Waiting[i];
+            }
+        
+        }
+    }
+}
